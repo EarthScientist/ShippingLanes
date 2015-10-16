919954cbbaec3d0d20ac5473b75fe31af08d74fc
EarthScientist <lindgren.mike@gmail.com>
2015-10-16 09:36:40 -0800

    modifies docstring

--- ais_shipping_voyage_splitter_phase3.py
+++ ais_shipping_voyage_splitter_phase3.py
@@ -18,21 +18,21 @@ def rolling_window( a, window ):
 	return np.lib.stride_tricks.as_strided( a, shape=shape, strides=strides )
 def calculate_distance(lat1, lon1, lat2, lon2, **kwargs):
 	'''
-	 Calculates the distance between two points given their (lat, lon) co-ordinates.
-	 It uses the Spherical Law Of Cosines (http://en.wikipedia.org/wiki/Spherical_law_of_cosines):
-	
-	 cos(c) = cos(a) * cos(b) + sin(a) * sin(b) * cos(C)                        (1)
-	
-	 In this case:
-	 a = lat1 in radians, b = lat2 in radians, C = (lon2 - lon1) in radians
-	 and because the latitude range is  [-π/2, π/2] instead of [0, π]
-	 and the longitude range is [-π, π] instead of [0, 2π]
-	 (1) transforms into:
-	
-	 x = cos(c) = sin(a) * sin(b) + cos(a) * cos(b) * cos(C)
-	
-	 Finally the distance is arccos(x)
-	 stolen from: https://gmigdos.wordpress.com/2010/03/31/python-calculate-the-distance-between-2-points-given-their-coordinates/
+	Calculates the distance between two points given their (lat, lon) co-ordinates.
+	It uses the Spherical Law Of Cosines (http://en.wikipedia.org/wiki/Spherical_law_of_cosines):
+
+	cos(c) = cos(a) * cos(b) + sin(a) * sin(b) * cos(C)                        (1)
+
+	In this case:
+	a = lat1 in radians, b = lat2 in radians, C = (lon2 - lon1) in radians
+	and because the latitude range is  [-π/2, π/2] instead of [0, π]
+	and the longitude range is [-π, π] instead of [0, 2π]
+	(1) transforms into:
+
+	x = cos(c) = sin(a) * sin(b) + cos(a) * cos(b) * cos(C)
+
+	Finally the distance is arccos(x)
+	borrowed from: https://gmigdos.wordpress.com/2010/03/31/python-calculate-the-distance-between-2-points-given-their-coordinates/
 	'''
 	import math
 	if ((lat1 == lat2) and (lon1 == lon2)):
