c0f5f80cfbf81b06cba1979d35835528c43e5012
EarthScientist <lindgren.mike@gmail.com>
2015-12-18 14:28:55 -0900

    adds a supplemental file that shows what fields to retain.

---
+++ newfields_temp_txt_shipping.py
@@ -0,0 +1,16 @@
+
+# Shipping Lanes -- New Fields 
+Voyage = unique id for a single voyage
+Direction = compass bearing (0.0-360.0)
+simple_direction = compass bearing grouped into {'NE':(-1.0,90.0),'SE':(90.0,180.0),'SW':(180.0,270.0),'NW':(270.0,361.0)}
+lon_begin = longitude value of the starting point of the transect
+lon_end = longitude value of the ending point of the transect
+lat_begin = latitude value of the starting point of the transect
+lat_end = latitude value of the ending point of the transect
+time_begin = time of stating point of the transect
+time_end = time of ending point of the transect
+bear_begin = bearing between the initial 2 points of the transect
+bear_end = bearing between the ending 2 points of the transect
+dir_begin = simple direction between the initial 2 points of the transect
+dir_end = simple direction between the ending 2 points of the transect
+
